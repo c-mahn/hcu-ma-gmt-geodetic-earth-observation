6113b5b078bf281680171e9b9d54fb5017b89385
wolfjoshua <64137154+wolfjoshua@users.noreply.github.com>
2023-01-21 14:05:43 +0100

    Update data_plotting.py

--- task_02/data_plotting.py
+++ task_02/data_plotting.py
@@ -54,24 +54,24 @@ def plot_sherical_harmonics(file_name="test",
         show_plot (bool): If True, the plot is shown. If False, the plot is only saved to a png file.
     """
 
-    """    os.system(f'gmt gmtset FORMAT_GEO_MAP ddd')  # Set the format of the map
-    os.system(f'set ascii_file={file_name}.txt')  # Set the ascii file
-    os.system(f'set grid_file={file_name}.grd')  # Set the grid file
-    os.system(f'gmt begin {file_name} {img_type}')  # Start the plot
-    os.system(f'gmt xyz2grd %ascii_file% -R{region} -r -I{grid_resolution} -G%grid_file% -V')  # Convert the ascii file to a grid file
-    os.system(f'gmt grd2cpt %grid_file% -C{color_palette} -Z ')  # Create the color palette for the grid file
-    os.system(f'gmt grdimage -J{map_projection} -R{region} %grid_file% -Q')  # Plot the data
-    os.system(f'gmt psxy {file_name_poly} -W3,red') # Plot the polygon
-    os.system(f'gmt coast -Bxa5g5 -Bya5g5 -BWESN+t"{title}" -W0.25p,80/80/80 -Df -V ')  # Plot the coastlines and the title
-    os.system(f'gmt text -F+cBL+t"{subtitle}" -N -D6.65c/-1c') # Plot the subtitle
-    os.system(f'gmt text -F+cBL+t"{editors}" -N -D5.15c/-1.5c')  # Plot the editors
-    # os.system(f'gmt colorbar {colorbar_settings}')  # Plot the colorbar
+    os.system(f'gmt gmtset FORMAT_GEO_MAP ddd',     # Set the format of the map
+               'set ascii_file={file_name}.txt',    # Set the ascii file
+               'set grid_file={file_name}.grd',     # Set the grid file
+               'gmt begin {file_name} {img_type}',  # Start the plot
+               'gmt xyz2grd %ascii_file% -R{region} -r -I{grid_resolution} -G%grid_file% -V',           # Convert the ascii file to a grid file
+               'gmt grd2cpt %grid_file% -C{color_palette} -Z',                                          # Create the color palette for the grid file
+               'gmt grdimage -J{map_projection} -R{region} %grid_file% -Q',                             # Plot the grid file                    
+               'gmt psxy {file_name_poly} -W3,red',                                                     # Plot region polygon
+               'gmt coast -Bxa5g5 -Bya5g5 -BWESN+t"{title}" -W0.25p,80/80/80 -Df -N1/1.25p,black -V',   # Plot the coastline and the title
+               'gmt text -F+cBL+t"{subtitle}" -N -D6.65c/-1c',  # Plot the subtitle
+               'gmt text -F+cBL+t"{editors}" -N -D5.15c/-1.5c', # Plot the editors
+               'gmt colorbar {colorbar_settings}'               # Plot the colorbar
+               'gmt end')
+        
     if(show_plot):  # Show the plot, if specified
         os.system(f'gmt end show')  # Show the plot
     else:  # Otherwise, just save the plot
-        os.system(f'gmt end')  # Save the plot"""
-
-    os.system(f'gmt gmtset FORMAT_GEO_MAP ddd \n set ascii_file={file_name}.txt \n set grid_file={file_name}.grd \n gmt begin {file_name} {img_type} \n gmt xyz2grd %ascii_file% -R{region} -r -I{grid_resolution} -G%grid_file% -V \n gmt grd2cpt %grid_file% -C{color_palette} -Z \n gmt grdimage -J{map_projection} -R{region} %grid_file% -Q \n gmt psxy {file_name_poly} -W3,red \n gmt coast -Bxa5g5 -Bya5g5 -BWESN+t"{title}" -W0.25p,80/80/80 -Df -N1/1.25p,black -V \n gmt text -F+cBL+t"{subtitle}" -N -D6.65c/-1c \n gmt text -F+cBL+t"{editors}" -N -D5.15c/-1.5c \n gmt end')
+        os.system(f'gmt end')  # Save the plot
     
     # Move file to the plots folder
     shutil.move(os.path.join(f'{file_name}.{img_type}'), os.path.join('plots', f'{file_name}.{img_type}'))
